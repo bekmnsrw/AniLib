0e3a5967a81b124647294acc1200ed61aacc22ab
bekmnsrw <ilya.bekmansurov@gmail.com>
2023-11-21 23:49:14 +0300

    Check if detekt action failures on code style violation

--- app/src/main/java/com/bekmnsrw/anilib/MainActivity.kt
+++ app/src/main/java/com/bekmnsrw/anilib/MainActivity.kt
@@ -18,12 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AniLibTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { Greeting("Android") }
             }
         }
     }
